6057760e68115cb0ea9ee7869d4e85c6fe3b932c
varuchin <draumar52@gmail.com>
2015-11-23 18:35:28 +0300

    added concurrency

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,108 +1,153 @@
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by varuchin on 23.11.2015.
  */
-public class Server {
+public class Server
+{
+
+    private final static int PORT = 1414;
+    private Executor serverExecutor;
+
+    void run() throws IOException
+    {
+        serverExecutor = Executors.newSingleThreadExecutor();
+        ServerSocket ss = new ServerSocket(PORT);
+
+        while (true)
+        {
+            Socket s = ss.accept();
+            try {
+                final SocketProcessor processor = new SocketProcessor(s);
+                final ExecutorService executor = Executors.newFixedThreadPool(10);
+                executor.submit(processor.createResponse);
+            } catch (Throwable e) {
+            }
+        }
+    }
 
     public static void main(String[] args) throws Throwable
     {
-
-        ServerSocket ss = new ServerSocket(1313);
-        System.out.println("asf");
+        new Server().run();
 /**
- while(true)
+ ExecutorService executor = Executors.newCachedThreadPool();
+
+
+ while(true) {
+
+ try
  {
+
+ ServerSocket ss = new ServerSocket(PORT);
+ Socket s = ss.accept();
+
+ Executor executor1 = Executors.newSingleThreadExecutor();
+ System.err.println("Client accepted.");
+ executor1.execute(new SocketProcessor(s).createResponse);
+
+ }
+ catch (Exception e)
+ {
+ e.printStackTrace();
+ }
+
+ //executor.shutdown();
+ /**
  Socket s = ss.accept();
  System.err.println("Client accepted.");
- new Thread(new SocketProcessor(s).run).run();
- }
+ executor.submit(new SocketProcessor(s));
  **/
+        // new Thread(new SocketProcessor(s)).run();
     }
-    /*
-    * переписать с импользование thread pool*/
-    private static class SocketProcessor implements Runnable
-    {
 
-        private Socket s;
-        private InputStream is;
-        private OutputStream os;
-
-        private SocketProcessor(Socket s) throws Throwable
-        {
-            this.s = s;
-            this.is = s.getInputStream();
-            this.os = s.getOutputStream();
-        }
-
-
-        @Override
-       public void run()
-        {
-            try
-            {
-                Thread.sleep(10000);
-
-                //readInputHeaders();
-                writeResponse("<html>\n" +
-                        " <head>\n" +
-                        "  <title></title>\n" +
-                        "<style type=\"text/css\">\n" +
-                        ".styletest {\n" +
-                        "\tcolor: red; \n" +
-                        "\tfont-size:12px; \n" +
-                        "\tfont-family:Arial\n" +
-                        "}\n" +
-                        "</style>\n" +
-                        " </head>\n" +
-                        "<body>\n" +
-                        "<font class=\"styletest\">Hello World from Valery! </font>\n" +
-                        "</body>\n" +
-                        "</html>");
-            }
-            catch (Throwable t)
-            {
-                t.printStackTrace();
-            }
-            finally {
-                try
-                {
-                    s.close();
-                }
-                catch (Throwable t)
-                {
-                    t.printStackTrace();
-                }
-            }
-            System.err.println("Client processing finished.");
-        };
-
-        private void writeResponse(String s) throws Throwable
-        {
-            String response = "HTTP/1.1 200 OK\r\n" +
-                    "Server: varuchinServer/2015-11-16\r\n" +
-                    "Content-Type: text/html\n" +
-                    "Content-Length: " + s.length() + "\r\n" +
-                    "Connection: close\r\n\r\n";
-            String result = response + s; // переписать на StrBuilder
-            os.write(result.getBytes());
-            os.flush();
-        }
-
+}
 
 /*
-        private void readInputHeaders() throws Throwable {
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            while (true) {
-                String s = br.readLine();
-                if (s == null || s.trim().length() == 0) {
-                    break;
-                }
+* переписать с импользование thread pool*/
+class SocketProcessor
+{
+
+    private Socket s;
+    private InputStream is;
+    private OutputStream os;
+
+    public SocketProcessor(Socket s) throws Throwable {
+        this.s = s;
+        this.is = s.getInputStream();
+        this.os = s.getOutputStream();
+    }
+
+
+    Runnable createResponse = () ->
+    {
+
+        try {
+            Thread.sleep(1000);
+            readInputHeaders();
+            writeResponse("<html>\n" +
+                    " <head>\n" +
+                    "  <title></title>\n" +
+                    "<style type=\"text/css\">\n" +
+                    ".styletest {\n" +
+                    "\tcolor: red; \n" +
+                    "\tfont-size:12px; \n" +
+                    "\tfont-family:Arial\n" +
+                    "}\n" +
+                    "</style>\n" +
+                    " </head>\n" +
+                    "<body>\n" +
+                    "<font class=\"styletest\">Hello World from Valery! </font>\n" +
+                    "</body>\n" +
+                    "</html>");
+        } catch (Throwable t) {
+            t.printStackTrace();
+        } finally {
+            try {
+                s.close();
+            } catch (Throwable t) {
+                t.printStackTrace();
             }
         }
-        */
+        System.err.println("Client processing finished.");
+    };
+
+
+    private void writeResponse(String s) throws Throwable
+    {
+        StringBuilder Responsebuilder = new StringBuilder();
+
+        Responsebuilder.append("HTTP/1.1 200 OK\r\n");
+        Responsebuilder.append("Server: varuchinServer/2015-11-16\r\n");
+        Responsebuilder.append("Content-Type: text/html\n");
+        Responsebuilder.append("Content-Length: " + s.length() + "\r\n");
+        Responsebuilder.append("Connection: close\r\n\r\n");
+
+        String response = Responsebuilder.toString();
+
+        StringBuilder builder1 = new StringBuilder();
+        builder1.append(response);
+        builder1.append(s);
+        String result = builder1.toString(); // переписать на StrBuilder
+
+
+        os.write(result.getBytes());
+        os.flush();
+    }
+
+
+    private void readInputHeaders() throws Throwable
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        while (true) {
+            String s = br.readLine();
+            if (s == null || s.trim().length() == 0) {
+                break;
+            }
+        }
     }
 }
